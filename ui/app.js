3e7136a08b7c59f5d0c85efd8837bf5d9254e16e
Luca Giacometti <luca.giacometti@barsanti.edu.it>
2021-12-12 03:02:33 +0100

    Merged to main

--- ui/app.js
+++ ui/app.js
@@ -1,5 +1,5 @@
 const routes = [
-  { path: '/', component: home },
+  { path: '/home', component: home },
   { path: '/employee', component: employee },
   { path: '/ticket', component: ticket }
 ]
