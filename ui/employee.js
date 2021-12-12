ad2f520d8d08a6583fe4f5556cb2366b5652fc80
Luca Giacometti <luca.giacometti@barsanti.edu.it>
2021-12-12 16:44:55 +0100

    Update to main branch

--- ui/employee.js
+++ ui/employee.js
@@ -59,8 +59,8 @@ const employee = {
   </table>
   -->
 
-  <ul v-for="emp in employees" class="list-group list-group-flush">
-    <li class="list-group-item">
+  <ul class="list-group list-group-flush">
+    <li v-for="emp in employees" class="list-group-item">
       <div class="row">
         <div class="col">{{emp.EmployeeName + " " + emp.EmployeeSurname}}</div>
         <div class="col">{{emp.EmployeeEmail}}</div>
@@ -146,7 +146,7 @@ const employee = {
   },
   methods: {
     fetchData() {
-      axios.get(variables.API_URL + 'employee')
+      axios.get(variables.API_URL + '/employee')
         .then((response) => {
           this.employees = response.data;
         });
@@ -161,7 +161,7 @@ const employee = {
     createEmployee() {
       //@TODO qui non funziona, scegli tipologia di dato con cui fare calcoli
       if (this.DateOfBirth < new Date().toLocaleString() - 18) {
-        axios.post(variables.API_URL + "employee", {
+        axios.post(variables.API_URL + "/employee", {
           EmployeeName: this.EmployeeName,
           EmployeeSurname: this.EmployeeSurname,
           EmployeeEmail: this.EmployeeEmail,
